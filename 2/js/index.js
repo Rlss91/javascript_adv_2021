9130a246a9806dedba77c8c161e8984f77536a3c
rlss91 <rlss91@gmail.com>
2021-12-23 16:00:38 +0200

    promise

--- 2/js/index.js
+++ 2/js/index.js
@@ -4,6 +4,9 @@ const handleBtnClick = async () => {
   try {
     let number = await genNumber();
     console.log("number", number);
+    document.getElementById(
+      "mylist"
+    ).innerHTML += `<li class="list-group-item">${number}</li>`;
   } catch (error) {
     console.error(error);
   }
